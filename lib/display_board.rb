87c6444a77c215c60d0971a5b0dbf0aa02cb4e40
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-02-23 23:46:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  cell = "   "
+  row = cell + "|" + cell + "|" + cell
+  row_seperator = "-----------"
+  puts row
+  puts row_seperator
+  puts row
+  puts row_seperator
+  puts row
+end
